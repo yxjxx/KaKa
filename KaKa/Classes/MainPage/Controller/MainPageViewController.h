49b8d7a4ddbf6fdd77540aa729a9555e404d1445
yxjxx <i@yxjxx.com>
2016-02-24 18:53:31 +0800

    new MainPageViewController

---
+++ KaKa/Classes/MainPage/Controller/MainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainPageViewController.h
+//  KaKa
+//
+//  Created by yxj on 16/2/24.
+//  Copyright © 2016年 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainPageViewController : UIViewController
+
+@end
