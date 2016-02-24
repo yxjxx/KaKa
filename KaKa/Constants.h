a2ffc791ae4eaa5e0426f97aee8e60a6d114f947
yxjxx <i@yxjxx.com>
2016-02-24 20:02:12 +0800

    create Constants.h

---
+++ KaKa/Constants.h
@@ -0,0 +1,24 @@
+//
+//  Constants.h
+//  KaKa
+//
+//  Created by yxj on 16/2/24.
+//  Copyright © 2016年 yxj. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define kScreenWidth [[UIScreen mainScreen] bounds].size.width
+#define kScreenHeight [[UIScreen mainScreen] bounds].size.height
+
+#define kStatusBarHeight 20
+#define kMagicNumber 44
+
+#define kNormalHeight 44
+#define kIconW 50
+#define kIconH 50
+
+#define kBtnFont [UIFont systemFontOfSize:15.0f]
+
+#endif /* Constants_h */
