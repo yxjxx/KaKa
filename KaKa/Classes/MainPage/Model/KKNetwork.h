5c21b01c8f938c56ae803486d79101135708a073
yxjxx <i@yxjxx.com>
2016-03-02 21:46:29 +0800

    新增网络单例类，使用 Block 进行数据的传递

---
+++ KaKa/Classes/MainPage/Model/KKNetwork.h
@@ -0,0 +1,25 @@
+//
+//  KKNetwork.h
+//  KaKa
+//
+//  Created by yxj on 3/2/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^requestSuccessed)(NSDictionary *responseJson);
+//定义了一个requestSuccessed类型的代码块，返回值void，接受一个NSDictionary*类型的参数
+typedef void(^requestFailed)(NSString *failedStr);
+
+
+@interface KKNetwork : NSObject
+
++ (KKNetwork *)sharedInstance;
+
+- (void)getVideoArrayDictWithOrder:(NSString *)order
+                              page:(NSString *)page
+                 completeSuccessed:(requestSuccessed)successBlock
+                    completeFailed:(requestFailed)failedBlock;
+
+@end
---
+++ KaKa/Classes/MainPage/Model/KKNetwork.m
@@ -0,0 +1,41 @@
+//
+//  KKNetwork.m
+//  KaKa
+//
+//  Created by yxj on 3/2/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import "KKNetwork.h"
+#import "AFNetworking.h"
+
+@implementation KKNetwork
+
++ (KKNetwork *)sharedInstance{
+    static KKNetwork *sharedNetwork;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedNetwork = [[KKNetwork alloc] init];
+    });
+    return sharedNetwork;
+}
+
+- (void)getVideoArrayDictWithOrder:(NSString *)order page:(NSString *)page completeSuccessed:(requestSuccessed)successBlock completeFailed:(requestFailed)failedBlock{
+    
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    session.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    
+    params[@"order"] = order;
+    params[@"page"] = [NSString stringWithFormat:@"%@-%d", page, kPageSize];
+    
+    [session GET:kVideoServerAddress parameters:params progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             successBlock(responseObject);
+         } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             NSLog(@"Fail, Error: %@", error);
+         }];
+}
+
+@end
