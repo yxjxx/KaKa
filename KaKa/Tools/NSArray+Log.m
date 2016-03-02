2759d5e129919ec178ee3db8ac1d3767220a06f0
yxjxx <i@yxjxx.com>
2016-03-02 21:42:42 +0800

    让 NSLog 可以正常显示中文

---
+++ KaKa/Tools/NSArray+Log.m
@@ -0,0 +1,26 @@
+//
+//  NSArray+Log.m
+//  KaKa
+//
+//  Created by yxj on 3/2/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import "NSArray+Log.h"
+
+@implementation NSArray (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    
+    for (id obj in self) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }
+    
+    [strM appendString:@")\n"];
+    
+    return strM;
+}
+
+@end
