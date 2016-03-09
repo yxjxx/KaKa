4b76c78dec250c927a680689f61d90adfd367c46
yxjxx <i@yxjxx.com>
2016-03-10 00:17:07 +0800

    增加新类 KKFriendProfileVC, 显示 topview

---
+++ KaKa/Classes/Friends/Controller/KKFriendProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  KKFriendProfileViewController.h
+//  KaKa
+//
+//  Created by yxj on 3/9/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KKFriendModel.h"
+
+@interface KKFriendProfileViewController : UIViewController
+
+@property (nonatomic, strong) KKFriendModel *friendModel;
+
+@end
---
+++ KaKa/Classes/Friends/Controller/KKFriendProfileViewController.m
@@ -0,0 +1,119 @@
+//
+//  KKFriendProfileViewController.m
+//  KaKa
+//
+//  Created by yxj on 3/9/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import "KKFriendProfileViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+
+@interface KKFriendProfileViewController()
+
+@property (nonatomic, strong) UIView *topView;
+@property (nonatomic, strong) UIImageView *portraitImageView;
+@property (nonatomic, strong) UILabel *followingNumLabel;
+@property (nonatomic, strong) UILabel *followingStrLabel;
+@property (nonatomic, strong) UILabel *fansNumLabel;
+@property (nonatomic, strong) UILabel *fansStrLabel;
+
+@property (nonatomic, strong) UICollectionView *myVideoCollectionView;
+@property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
+@property (nonatomic, strong) NSMutableArray *myVideoArray;
+@property (nonatomic, assign) NSInteger pageNum;
+
+@end
+
+@implementation KKFriendProfileViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.navigationItem.title = self.friendModel.username;
+    
+    [self.view addSubview:self.topView];
+    [self.portraitImageView sd_setImageWithURL:[NSURL URLWithString:self.friendModel.portrait] placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    [self.topView addSubview:self.portraitImageView];
+    //TODO: followingNum数据修改
+    self.followingNumLabel.text = self.friendModel.fans;
+    [self.topView addSubview:self.followingNumLabel];
+    [self.topView addSubview:self.followingStrLabel];
+    self.fansNumLabel.text = self.friendModel.fans;
+    [self.topView addSubview:self.fansNumLabel];
+    [self.topView addSubview:self.fansStrLabel];
+}
+
+- (UIView *)topView{
+    if (_topView == nil) {
+        _topView = [[UIView alloc] init];
+        [_topView setFrame:CGRectMake(kMagicZero, kStatusBarHeight + kNavgationBarHeight, kScreenWidth, kProfileTopUIViewHeight)];
+        _topView.backgroundColor = [UIColor colorWithRed:46/256.0 green:46/256.0 blue:46/256.0 alpha:1];
+    }
+    return _topView;
+}
+
+- (UIImageView *)portraitImageView{
+    if (_portraitImageView == nil) {
+        _portraitImageView = [[UIImageView alloc] init];
+        _portraitImageView.size = CGSizeMake(85, 85);
+        _portraitImageView.center = CGPointMake(kScreenWidth/2, 60);
+        _portraitImageView.layer.cornerRadius = _portraitImageView.size.width/2;
+        _portraitImageView.layer.masksToBounds = YES;
+        //TODO: 添加边框
+    }
+    return _portraitImageView;
+}
+
+- (UILabel *)followingNumLabel{
+    if (_followingNumLabel == nil) {
+        _followingNumLabel = [[UILabel alloc] init];
+        [_followingNumLabel setFrame:CGRectMake(kScreenWidth/2-85, 40, 25, 20)];
+        _followingNumLabel.layer.cornerRadius = 2;
+        _followingNumLabel.layer.masksToBounds = YES;
+        _followingNumLabel.textAlignment = NSTextAlignmentCenter;
+        [_followingNumLabel setTextColor:[UIColor colorWithRed:243/256.0 green:233/256.0 blue:234/256.0 alpha:1]];
+        _followingNumLabel.backgroundColor = [UIColor colorWithRed:182/256.0 green:71/256.0 blue:72/256.0 alpha:1];
+    }
+    return _followingNumLabel;
+}
+
+- (UILabel *)followingStrLabel{
+    if (_followingStrLabel == nil) {
+        _followingStrLabel = [[UILabel alloc] init];
+        _followingStrLabel.frame = CGRectMake(kScreenWidth/2-90, 55, 45, 40);
+        _followingStrLabel.text = @"关注";
+        [_followingStrLabel setTextColor:[UIColor colorWithRed:243/256.0 green:233/256.0 blue:234/256.0 alpha:1]];
+    }
+    return _followingStrLabel;
+}
+
+- (UILabel *)fansNumLabel{
+    if (_fansNumLabel == nil) {
+        _fansNumLabel = [[UILabel alloc] init];
+        [_fansNumLabel setFrame:CGRectMake(kScreenWidth/2+65, 40, 25, 20)];
+        _fansNumLabel.layer.cornerRadius = 2;
+        _fansNumLabel.layer.masksToBounds = YES;
+        _fansNumLabel.textAlignment = NSTextAlignmentCenter;
+        [_fansNumLabel setTextColor:[UIColor colorWithRed:243/256.0 green:233/256.0 blue:234/256.0 alpha:1]];
+        _fansNumLabel.backgroundColor = [UIColor colorWithRed:182/256.0 green:71/256.0 blue:72/256.0 alpha:1];
+    }
+    return _fansNumLabel;
+}
+
+- (UILabel *)fansStrLabel{
+    if (_fansStrLabel == nil) {
+        _fansStrLabel = [[UILabel alloc] init];
+        _fansStrLabel.frame = CGRectMake(kScreenWidth/2+60, 55, 45, 40);
+        _fansStrLabel.text = @"粉丝";
+        [_fansStrLabel setTextColor:[UIColor colorWithRed:243/256.0 green:233/256.0 blue:234/256.0 alpha:1]];
+    }
+    return _fansStrLabel;
+}
+
+
+
+
+
+@end
