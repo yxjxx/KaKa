9af9ea6e7011e6c87221c7847ada6c455cc4b3ca
yxjxx <i@yxjxx.com>
2016-03-09 22:07:38 +0800

    KKFriendTableViewCell

--- KaKa/Classes/Friends/View/KKFriendTableViewCell.h
+++ KaKa/Classes/Friends/View/KKFriendTableViewCell.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "KKFriendModel.h"
 
 @interface KKFriendTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) KKFriendModel *friendModel;
 
 @end
--- KaKa/Classes/Friends/View/KKFriendTableViewCell.m
+++ KaKa/Classes/Friends/View/KKFriendTableViewCell.m
@@ -7,7 +7,67 @@
 //
 
 #import "KKFriendTableViewCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+@interface KKFriendTableViewCell()
+
+@property (nonatomic, strong) UIImageView *portraitImageView;
+@property (nonatomic, strong) UILabel *usernameLabel;
+@property (nonatomic, strong) UILabel *videoNumberLabel;
+
+@end
 
 @implementation KKFriendTableViewCell
+
+- (UIImageView *)portraitImageView{
+    if (_portraitImageView == nil) {
+        _portraitImageView = [[UIImageView alloc] init];
+        _portraitImageView.frame = CGRectMake(0, 0, 55, 60);
+        [self.contentView addSubview:_portraitImageView];
+    }
+    return _portraitImageView;
+}
+
+- (UILabel *)usernameLabel{
+    if (_usernameLabel == nil) {
+        _usernameLabel = [[UILabel alloc] init];
+        _usernameLabel.frame = CGRectMake(60, 5, 200, 30);
+        [self.contentView addSubview:_usernameLabel];
+    }
+    return _usernameLabel;
+}
+
+- (UILabel *)videoNumberLabel{
+    if (_videoNumberLabel == nil) {
+        _videoNumberLabel = [[UILabel alloc] init];
+//TODO: adjust frame for this cell
+        _videoNumberLabel.size = CGSizeMake(200, 60);
+        _videoNumberLabel.center = CGPointMake(kScreenWidth-100, 30);
+        _videoNumberLabel.textAlignment = NSTextAlignmentRight;
+//        _videoNumberLabel.frame = CGRectMake(60, 30, 200, 30);
+        [self.contentView addSubview:_videoNumberLabel];
+    }
+    return _videoNumberLabel;
+}
+
+- (void)setFriendModel:(KKFriendModel *)friendModel{
+    _friendModel = friendModel;
+    [self setData];
+    [self setUI];
+}
+
+- (void)setData{
+    [self.portraitImageView sd_setImageWithURL:[NSURL URLWithString:self.friendModel.portrait] placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    self.usernameLabel.text = self.friendModel.username;
+    self.videoNumberLabel.text = [NSString stringWithFormat:@"%@ followers", self.friendModel.video];
+}
+
+- (void)setUI{
+    self.contentView.backgroundColor = [UIColor colorWithRed:45/256.0 green:45/256.0 blue:45/256.0 alpha:1];
+
+}
+
+
+
 
 @end
