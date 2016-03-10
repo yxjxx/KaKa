7a515990754a1548b5e4c93211d7e8c751ad9610
yxjxx <i@yxjxx.com>
2016-03-10 17:26:41 +0800

    新建 bottomView

---
+++ KaKa/Classes/Others/View/KKPlayVideoVCBottomView.h
@@ -0,0 +1,15 @@
+//
+//  KKPlayVideoVCBottomView.h
+//  KaKa
+//
+//  Created by yxj on 3/10/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKPlayVideoVCBottomView : UIView
+
+
+
+@end
---
+++ KaKa/Classes/Others/View/KKPlayVideoVCBottomView.m
@@ -0,0 +1,20 @@
+//
+//  KKPlayVideoVCBottomView.m
+//  KaKa
+//
+//  Created by yxj on 3/10/16.
+//  Copyright © 2016 yxj. All rights reserved.
+//
+
+#import "KKPlayVideoVCBottomView.h"
+
+@interface KKPlayVideoVCBottomView()
+
+//点赞数zan， 播放量hint，时长 timelen，收藏 favorit， 头像imageview， 用户名username
+//@property (nonatomic, strong) UILabel *<#name#>;
+
+@end
+
+@implementation KKPlayVideoVCBottomView
+
+@end
